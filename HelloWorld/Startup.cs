f85ab81299072304f82c2f19a2d9d8d3887c370e
Roman Kreisel <mail@romankreisel.de>
2017-12-22 11:05:14 +0100

    changes

---
+++ HelloWorld/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace HelloWorld
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment hostingEnvironment){
+        }
+
+        public void Configure(IApplicationBuilder applicationBuilder, IHostingEnvironment hostingEnvironment, ILoggerFactory loggerFactory)
+        {
+            applicationBuilder.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World");
+            });
+        }
+    }
+}
